c8176f1d86c3382311a5b34192fc8c547fd2b524
rc420head <dennislebhardt@yahoo.com>
2014-09-24 00:16:11 -0500

    i think i got it now PPP

--- device_ville.mk
+++ device_ville.mk
@@ -90,8 +90,8 @@ $(call inherit-product-if-exists, vendor/htc/ville/ville-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_DEVICE := ville
-PRODUCT_NAME := ville
+PRODUCT_DEVICE := ville215
+PRODUCT_NAME := ville215
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One S
 PRODUCT_MANUFACTURER := HTC
